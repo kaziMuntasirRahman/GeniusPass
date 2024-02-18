c25e25361d08173969b719c5c024328a8d3063d4
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-02-18 18:21:20 +0600

    remaining seats section done, Alhamdulillah

--- js/price-row-add.js
+++ js/price-row-add.js
@@ -4,8 +4,15 @@ const totalPriceText = document.getElementById('total-price');
 let totalPrice = parseInt(totalPriceText.innerText);
 const grandPriceText = document.getElementById('grand-total');
 let grandPrice = parseInt(grandPriceText.innerText);
+const remainingSeatsText = document.getElementById('remaining-seat');
+let remainingSeats = parseInt(remainingSeatsText.innerText);
 
 function addNewRow(button) {
+  if (button.getAttribute('data-clicked') === 'true') {
+    alert('This seat has already been booked.');
+    return;
+  }
+
   if (numberOfSeat === 4) {
     alert('You can maximum buy 4 tickets at a same time.');
     return;
@@ -23,6 +30,11 @@ function addNewRow(button) {
     bookedCartDiv.appendChild(paragraph);
     paragraph.classList.add('text-gray-950', 'text-opacity-60', 'font-normal')
   }
+
+  button.setAttribute('data-clicked', 'true');
+
+  remainingSeats--;
+  remainingSeatsText.innerText = remainingSeats;
   numberOfSeat++;
   numberOfSeatText.innerText = numberOfSeat;
   totalPrice += 550;
