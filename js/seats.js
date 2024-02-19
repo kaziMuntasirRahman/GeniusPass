25600bb3edcb5d9fb543f0ec1420145ce8170486
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-02-19 21:31:54 +0600

    seat section done, Alhamdulillah

--- js/seats.js
+++ js/seats.js
@@ -5,7 +5,7 @@ const paragraph = document.querySelectorAll('.seats p');
 
 document.addEventListener("DOMContentLoaded", function() {
   buttons.forEach(function(button) {
-    button.classList.add('w-28', 'h-14', 'bg-stone-200', 'hover:bg-white', 'hover:font-bold', 'hover:border', 'hover:border-black',  'rounded-xl', 'text-gray-950', 'text-opacity-50', 'text-lg', 'font-medium', 'flex', 'justify-center', 'items-center');
+    button.classList.add('w-24', 'lg:w-28', 'py-4', 'bg-stone-200', 'hover:bg-white', 'hover:font-bold', 'hover:border', 'hover:border-black',  'rounded-xl', 'text-gray-950', 'text-opacity-50', 'text-lg', 'font-medium', 'flex', 'justify-center', 'items-center');
     button.style.fontFamily="Inter";
   });
 
