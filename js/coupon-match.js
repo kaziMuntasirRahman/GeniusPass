aaf11882c644426f8d049561b9a8131070a04362
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-02-19 23:16:41 +0600

    really this is the last time

--- js/coupon-match.js
+++ js/coupon-match.js
@@ -8,10 +8,12 @@ function applyCoupon() {
     grandPrice = totalPrice - (totalPrice * 15 / 100);
     grandPriceText.innerText = grandPrice;
     document.getElementById('coupon-level').classList.add('hidden');
+    document.getElementById('grand-total-container').classList.add("bg-teal-200", "px-1", "py-2", "rounded-md");
   } else if (couponValue === coupon2) {
     grandPrice = totalPrice - (totalPrice * 20 / 100);
     grandPriceText.innerText = grandPrice;
     document.getElementById('coupon-level').classList.add('hidden');
+    document.getElementById('grand-total-container').classList.add("bg-teal-200", "px-1", "py-2", "rounded-md");
   } else {
     alert("Wrong Coupon Code. Please Enter Correctly. Coupon Codes are case sensitive");
   }
